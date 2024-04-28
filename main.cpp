4586a436bab73423d85e5c7e2c6164d86f13f574
Renan Augusto <renancandido82@outlook.com>
2024-04-28 10:52:16 -0300

    Catnet - Add ip address and improvments.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <cstdlib>
 #include <fstream>
-#include <ostream>
 #include <string>
 #include <cstring>
 #include <winsock2.h>
@@ -9,6 +8,7 @@
 #include <unordered_map>
 #include <vector>
 #include <windows.h>
+#include <sstream>
 using namespace std;
 
 struct AstElement{
@@ -33,7 +33,8 @@ unordered_map<string, variables> variableTable;
 AstElement* parser(string expression, int childrenLine) {
     AstElement* element = new AstElement();
     variables var;
-    if(expression.find("var") != string::npos) {
+    int intoroute = 1;
+    if(expression.find("var") != string::npos && intoroute == 1) {
         size_t id1 = expression.find("var") + 3;
         size_t id2 = expression.find("=");
         var.name = expression.substr(id1 + 1, id2 - id1 - 2);
@@ -71,22 +72,23 @@ AstElement* parser(string expression, int childrenLine) {
             element->params = expression.substr(pos1 + 1, pos2 - pos1 - 1);
             element->completeLine = expression;
             element->action = expression.substr(p1 + 1, p2 - p1 - 1);
+            intoroute = 0;
         }
     }
-        if(expression.find("printLog") != string::npos) {
+        if(expression.find("printLog") != string::npos && intoroute == 1) {
             size_t pos1 = expression.find("(");
             size_t pos2 = expression.find(")");
             element->name = "printLog";
             element->line = childrenLine;
             element->type = "printLogConsole";
-            element->params = expression.substr(pos1 + 2, pos2 - pos1 - 2);
+            element->params = expression.substr(pos1 + 2, pos2 - pos1 - 3);
             element->completeLine = expression;
         }
-        if(expression.find("readFile") != string::npos) {
+        if(expression.find("readFile") != string::npos && intoroute == 1) {
             size_t pos1 = expression.find("(");
-            size_t pos2 = expression.find(")");
+            size_t pos2 = expression.find(";");
             ifstream file;
-            file.open(expression.substr(pos1 + 2, pos2 - pos1 - 3));
+            file.open(expression.substr(pos1 + 2, pos2 - pos1 - 4));
             string line, text;
             if(file.is_open()) {
                 while(getline(file, line)) {
@@ -138,7 +140,7 @@ AstElement* parser(string expression, int childrenLine) {
     return windowcode;
 }*/
 SOCKET serverSocket;
-int openSocket(string port) {
+int openSocket(string address, string port) {
     WSADATA wsaData;
     sockaddr_in serverAddr;
     if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
@@ -151,7 +153,11 @@ int openSocket(string port) {
     
     }
     serverAddr.sin_family = AF_INET;
-    serverAddr.sin_addr.s_addr = INADDR_ANY; 
+    if(address == "any"){
+        serverAddr.sin_addr.s_addr = INADDR_ANY;
+    } else {
+        serverAddr.sin_addr.s_addr = inet_addr(address.c_str());
+    }
     serverAddr.sin_port = htons(stoi(port));
     
     if (bind(serverSocket, (sockaddr*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR) {
@@ -171,15 +177,32 @@ int openSocket(string port) {
 bool ignoreThis = false;
 SOCKET clientSocket;
 string route;
+bool firstRound = false;
 int startRuntime(vector<AstElement*> elementsVector) {
     for(const auto& elements : elementsVector) {
         if(elements->type == "startServer"){
             if(ignoreThis == false) {
-                int result = openSocket(elements->params);
-                if(result != 0) {
-                    return 1;
+                if(elements->params.find(",")){
+                    string params = elements->params;
+                    size_t pos1 = params.find(",");
+                    size_t pos2 = params.find(")");
+                    size_t ps1 = params.find("(");
+                    size_t ps2 = params.find(",");
+                    string port = params.substr(pos1 + 1, pos2 - pos1 - 1);
+                    string address = params.substr(ps1 + 1, ps2 - ps1 - 1);
+                    int result = openSocket(address, port);
+                
+                    if(result != 0) {
+                        return 1;
+                    }
+                    ignoreThis = true;
+                } else {
+                    int result = openSocket("any", elements->params);            
+                    if(result != 0) {
+                        return 1;
+                    }
+                    ignoreThis = true;
                 }
-                ignoreThis = true;
             }
             clientSocket  = accept(serverSocket, nullptr, nullptr);
             if (clientSocket == INVALID_SOCKET) {
@@ -207,22 +230,38 @@ int startRuntime(vector<AstElement*> elementsVector) {
             }
         }else if(elements->type == "sendResponse") {
             string response;
-            if(!variableTable[elements->params].name.empty()) {
-                response = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n" + variableTable[elements->params].value + "\r\n";
+            string type = "text/html";
+            string searchTerm = elements->params;
+            if(elements->params.find(",") != string::npos) {
+                string params = elements->params;
+                size_t pos1 = params.find(',');
+                size_t pos2 = params.find(')');
+                size_t pose1 = params.find('(');
+                size_t pose2 = params.find(',');
+                type = params.substr(pos1 + 2, pos2 - pos1 - 3);
+                searchTerm = params.substr(pose1 + 1, pose2 - pose1 - 1);
+            }
+            if(!variableTable[searchTerm].name.empty()) {
+                response = "HTTP/1.1 200 OK\r\nContent-Type: " + type + "\r\n\r\n" + variableTable[searchTerm].value + "\r\n";
             } else{
-                response = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n" + elements->params + "\r\n";
+                response = "HTTP/1.1 200 OK\r\nContent-Type: " + type + "\r\n\r\n" + elements->params + "\r\n";
             }
             send(clientSocket, response.c_str(), response.size(), 0);
             closesocket(clientSocket);
-        }else if(elements->type == "printLogConsole") {
+        }else if(elements->type == "printLogConsole" && firstRound == false) {
             cout << elements->params << endl;
+            firstRound = true;
         } else if(elements->type == "checkRoute") {
             if(route == elements->params) {
                 vector<AstElement*> elementVector;
                 AstElement* element;
-                element = parser(elements->action, 0);
-                if(!element->name.empty()) {
-                    elementVector.push_back(element);
+                string line;
+                istringstream stream(elements->action);
+                while(getline(stream, line)) {
+                    element = parser(line, 0);
+                    if(!element->name.empty()) {
+                        elementVector.push_back(element);
+                    }
                 }
                 startRuntime(elementVector);
             }
@@ -244,7 +283,6 @@ int main(int argc, char *argv[]) {
         ifstream file;
         const char* nameArchiveC = argv[1];
         file.open(nameArchiveC);
-        ofstream codeFinal("codeFinal.asm");
         int lineCout = 1;
         if(file.is_open()){
             int childrenLine;
